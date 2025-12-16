e99a3465dfbb1c62f856b4f066c0388ea81441aa
AbhishekPatil1310 <ap7497493@gmail.com>
2025-12-16 07:01:11 +0530

    Add new allowed origins for CORS

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -18,7 +18,10 @@ const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
-    "https://your-prod-domain.com"
+    "https://task-management-frontend-uhsr.vercel.app",
+  "https://task-management-frontend-uhsr-abhisheks-projects-680a2fd9.vercel.app",
+  "https://task-management-frontend-git-bf3603-abhisheks-projects-680a2fd9.vercel.app",
+  "https://task-management-frontend-uhsr-n19swj537.vercel.app"
 ];
 app.use((0, cors_1.default)({
     origin: allowedOrigins,
